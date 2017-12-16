1b7a187ac68a512107df0640a53b23cd068107b0
Michael Flis <michaelflis07@gmail.com>
2017-12-16 15:51:30 -0800

    Complete queue prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+
+  newQueue.storage = {};
+  newQueue.start = 0;
+  newQueue.end = 0;
+
+  return newQueue;
+
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (value) {
+  this.storage[this.end++] = value;
+};
+
+
+queueMethods.dequeue = function () {
+  var dequeued = this.storage[this.start];
+  if ( this.size() ) {
+    delete this.storage[this.start++];
+    return dequeued;
+  }
+};
+
+
+queueMethods.size = function () {
+  return this.end - this.start;
+};
 
 
