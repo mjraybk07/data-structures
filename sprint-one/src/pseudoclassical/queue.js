09eaa894b2d3af574b0de0e437f8ba002ee4949d
Michael Flis <michaelflis07@gmail.com>
2017-12-16 16:13:43 -0800

    Complete queue pseudoclassical, Complete sprint-one solo

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.start = 0;
+  this.end = 0;
 };
 
+Queue.prototype.enqueue = function (value) {
+  this.storage[this.end++] = value;
+};
+
+Queue.prototype.dequeue = function () {
+  var dequeued = this.storage[this.start];
+  if ( this.size() ) {
+    delete this.storage[this.start++];
+    return dequeued;
+  }
+};
+
+Queue.prototype.size = function () {
+  return this.end - this.start;
+};
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -22,4 +22,3 @@ Stack.prototype.size = function () {
   return this.count;
 };
 
-
