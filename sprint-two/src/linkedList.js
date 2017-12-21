18ed743b3471e98c09e59920d9dc590e0296498f
Michael Flis <michaelflis07@gmail.com>
2017-12-21 13:43:06 -0800

    Add linkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,13 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if ( list.head === null && list.tail === null ) {
+      list.head = Node(value);
+      list.tail = Node(value);
+    } else if ( list.head.next === null ) {
+      list.tail = Node(value);
+      list.head.next = Node(value);
+    }
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    var oldHead = list.head;
+    var newHead = list.head.next;
+    delete list.head;
+    list.head = newHead;
+    return oldHead.value;
+
   };
 
   list.contains = function(target) {
+    var node = list.head;
+
+    while ( node ) {
+      if ( node.value === target ) {
+        return true;
+      } else {
+        node = node.next;
+      }
+    }
+    return false;
   };
+
 
   return list;
 };
@@ -26,4 +51,9 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ addToTail =>  O(1) Constant Time
+ removeHead => O(1) Constant Time
+ contains =>   O(n) Linear Time
+
  */
